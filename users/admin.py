045cc094a728c166d64193dbd79213ef6e1fbfce
김종건 <48309937+kjg9704@users.noreply.github.com>
2021-03-12 22:45:35 +0900

    user커스텀

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
-from .models import User
-# Register your models here.
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-admin.site.register(User)
+from .forms import UserChangeForm, UserCreationForm
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'nickName', 'date_of_birth', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('nickName', 'date_of_birth')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nickName', 'date_of_birth', 'password1', 'password2')}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
